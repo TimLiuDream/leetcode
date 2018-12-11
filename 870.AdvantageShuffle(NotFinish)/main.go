9d1d90f9b769b81910dc90dc3144f9b846fe815f
TimLiu <417806161@qq.com>
2018-12-11 15:25:28 +0800

    优势洗牌（未完成）

---
+++ 870.AdvantageShuffle(NotFinish)/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func advantageCount(A []int, B []int) []int {
+	return A
+}
+
+func main() {
+	a := []int{2, 7, 11, 15}
+	b := []int{1, 10, 4, 11}
+	fmt.Println(advantageCount(a, b))
+	a1 := []int{12, 24, 8, 32}
+	b1 := []int{13, 25, 32, 11}
+	fmt.Println(advantageCount(a1, b1))
+}
