189fe321e00196efee21938472fbc05bbf20ca56
TimLiu <liufotian@ones.ai>
2019-01-08 23:01:49 +0800

    买卖股票问题

---
+++ 122.BuySellStock/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	result := 0
+	for index := 0; index < len(prices)-1; index++ {
+		if prices[index+1] > prices[index] {
+			result += prices[index+1] - prices[index]
+		}
+	}
+	return result
+}
+
+func main() {
+	nums := []int{7, 1, 5, 3, 6, 4}
+	nums1 := []int{1, 2, 3, 4, 5}
+	nums2 := []int{7, 6, 4, 3, 1}
+	fmt.Println(maxProfit(nums))
+	fmt.Println(maxProfit(nums1))
+	fmt.Println(maxProfit(nums2))
+}
