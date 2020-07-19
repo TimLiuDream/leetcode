010b310fbccbdf9d7d46ddc59aeffd1abd66d0d7
timliu <timliudream@163.com>
2020-07-19 22:37:55 +0800

    update:136

--- 136.SingleNumber/main.go
+++ 136.SingleNumber/main.go
@@ -21,9 +21,22 @@ func singleNumber(nums []int) int {
 	return result
 }
 
+func singleNumber1(nums []int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	res := nums[0]
+	for i := 1; i < len(nums); i++ {
+		res = res ^ nums[i]
+	}
+	return res
+}
+
 func main() {
 	nums1 := []int{2, 2, 1}
 	fmt.Println(singleNumber(nums1))
+	fmt.Println(singleNumber1(nums1))
 	nums2 := []int{4, 1, 2, 1, 2}
 	fmt.Println(singleNumber(nums2))
+	fmt.Println(singleNumber1(nums2))
 }
