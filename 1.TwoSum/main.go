d22b4b47c2ba40f1446093b323773e08fbca9064
timliu <timliudream@163.com>
2021-03-23 23:30:42 +0800

    remove:dir

--- 1.TwoSum/main.go
+++ 1.TwoSum/main.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 
-//给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+// Label 数组 哈希表
+
+// 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
