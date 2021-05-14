f8bc9632502519e6c94d15761c63861de85f06ce
liufotian <timliudream@163.com>
2021-05-14 11:39:07 +0800

    add:12

---
+++ 12.IntegertoRoman/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// Label 数学
+// Label 字符串
+func main() {
+	num := 3
+	fmt.Println(intToRoman(num))
+}
+
+var valueSymbols = []struct {
+	value  int
+	symbol string
+}{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func intToRoman(num int) string {
+	roman := []byte{}
+	for _, vs := range valueSymbols {
+		for num >= vs.value {
+			num -= vs.value
+			roman = append(roman, vs.symbol...)
+		}
+		if num == 0 {
+			break
+		}
+	}
+	return string(roman)
+}
