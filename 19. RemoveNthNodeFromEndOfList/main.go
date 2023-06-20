e88e64103eb1f081e9f28645af6bd114b11f666b
liufotian <timliudream@163.com>
2023-06-20 14:20:34 +0800

    add:19

---
+++ 19. RemoveNthNodeFromEndOfList/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// [1,2,3,4,5
+	head := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+	head = removeNthFromEnd(head, 2)
+	fmt.Println(head)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	nodes := []*ListNode{}
+	dummy := &ListNode{0, head}
+	for node := dummy; node != nil; node = node.Next {
+		nodes = append(nodes, node)
+	}
+	pre := nodes[len(nodes)-n-1]
+	pre.Next = pre.Next.Next
+	return dummy.Next
+}
+
+func getLength(head *ListNode) (length int) {
+	for ; head != nil; head = head.Next {
+		length++
+	}
+	return
+}
+
+func removeNthFromEnd1(head *ListNode, n int) *ListNode {
+	length := getLength(head)
+	dummy := &ListNode{0, head}
+	cur := dummy
+	for i := 0; i < length-n; i++ {
+		cur = cur.Next
+	}
+	cur.Next = cur.Next.Next
+	return dummy.Next
+}
