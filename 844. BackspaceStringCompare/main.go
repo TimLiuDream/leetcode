8aca53bb491bad36980ac9962d7f3b1ae954f107
liufotian <timliudream@163.com>
2023-06-21 20:04:54 +0800

    add:844

---
+++ 844. BackspaceStringCompare/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1, t1 := "ab#c", "ad#c"
+	fmt.Println(backspaceCompare(s1, t1))
+
+	s2, t2 := "ab##", "c#d#"
+	fmt.Println(backspaceCompare(s2, t2))
+
+	s3, t3 := "a#c", "b"
+	fmt.Println(backspaceCompare(s3, t3))
+}
+
+func backspaceCompare(s string, t string) bool {
+	buildStack := func(str string) []string {
+		stack := make([]string, 0)
+		for _, raw := range str {
+			c := string(raw)
+			if c != "#" {
+				stack = append(stack, c)
+			} else {
+				if len(stack) == 0 {
+					continue
+				}
+				stack = stack[:len(stack)-1]
+			}
+		}
+		return stack
+	}
+	sStack := buildStack(s)
+	tStack := buildStack(t)
+	if len(sStack) == len(tStack) {
+		for i := 0; i < len(sStack); i++ {
+			if sStack[i] != tStack[i] {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
