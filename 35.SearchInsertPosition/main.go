c1dc6030d668ba367a8019975092ea9d1b16b3fa
liufotian <timliudream@163.com>
2023-06-30 13:58:29 +0800

    update:35

--- 35.SearchInsertPosition/main.go
+++ 35.SearchInsertPosition/main.go
@@ -18,10 +18,35 @@ func searchInsert(nums []int, target int) int {
 	return result
 }
 
+func searchInsert1(nums []int, target int) int {
+	if nums[len(nums)-1] < target {
+		return len(nums)
+	}
+	start, end := 0, len(nums)-1
+	for start <= end {
+		mid := (start + end) >> 1
+		if nums[mid] >= target {
+			if mid == 0 || nums[mid-1] < target {
+				return mid
+			} else {
+				end = mid - 1
+			}
+		} else {
+			start = mid + 1
+		}
+	}
+	return -1
+}
+
 func main() {
 	nums := []int{1, 3, 5, 6}
 	fmt.Println(searchInsert(nums, 5))
 	fmt.Println(searchInsert(nums, 2))
 	fmt.Println(searchInsert(nums, 7))
 	fmt.Println(searchInsert(nums, 0))
+
+	fmt.Println(searchInsert1(nums, 5))
+	fmt.Println(searchInsert1(nums, 2))
+	fmt.Println(searchInsert1(nums, 7))
+	fmt.Println(searchInsert1(nums, 0))
 }
