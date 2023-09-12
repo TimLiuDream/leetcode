4c2e424ab5b0b45addbbf835b8787897afb1dd67
tim <timliudream@163.com>
2023-09-12 16:40:54 +0800

    update: 26 使用 map 以及计数器解决

--- 26.RemoveDuplicates/main.go
+++ 26.RemoveDuplicates/main.go
@@ -2,36 +2,38 @@ package main
 
 import "fmt"
 
+// 双指针解法
 func removeDuplicates(nums []int) int {
-	//如果是空切片，那就返回0
 	if len(nums) == 0 {
 		return 0
 	}
 	//用两个标记来比较相邻位置的值
 	//当一样的话，那就不管继续
 	//当不一样的时候，就把right指向的值赋值给left下一位
-	left, right := 0, 1
-	for ; right < len(nums); right++ {
+	left, right := 0, 0
+	for right < len(nums)-1 {
+		right++
 		if nums[left] == nums[right] {
 			continue
 		}
 		left++
 		nums[left] = nums[right]
 	}
-	fmt.Println(nums[:left+1])
 	return left + 1
 }
 
-//不考虑空间复杂度
-//引入map，把不一样的放进去
+// 使用 map 和计数器
 func removeDuplicates1(nums []int) int {
-	m := make(map[int]int)
-	for _, v := range nums {
-		if _, ok := m[v]; !ok {
-			m[v] = v
+	m := make(map[int]struct{})
+	counter := 0 // 记录有多少个不一致的数字
+	for _, num := range nums {
+		if _, ok := m[num]; !ok {
+			m[num] = struct{}{}
+			nums[counter] = num
+			counter++
 		}
 	}
-	return len(m)
+	return counter
 }
 
 func main() {
@@ -39,4 +41,17 @@ func main() {
 	fmt.Println(removeDuplicates(nums))
 	nums1 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 	fmt.Println(removeDuplicates(nums1))
+	nums2 := []int{1, 1}
+	fmt.Println(removeDuplicates(nums2))
+	nums3 := []int{}
+	fmt.Println(removeDuplicates(nums3))
+	fmt.Println("----------------------------")
+	nums = []int{1, 1, 2}
+	fmt.Println(removeDuplicates1(nums))
+	nums1 = []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates1(nums1))
+	nums2 = []int{1, 1}
+	fmt.Println(removeDuplicates1(nums2))
+	nums3 = []int{}
+	fmt.Println(removeDuplicates1(nums3))
 }
