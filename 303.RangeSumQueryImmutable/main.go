524db6d78b20de1a7ec82f7cdd0e4a0deac4802c
tim <timliudream@163.com>
2023-09-12 16:52:40 +0800

    update: 303 使用最简单的构造方式解决

--- 303.RangeSumQueryImmutable/main.go
+++ 303.RangeSumQueryImmutable/main.go
@@ -4,37 +4,56 @@ import "fmt"
 
 func main() {
 	nums := []int{-2, 0, 3, -5, 2, -1}
-	// i := 0
-	// j := 2
-	// obj := Constructor(nums)
-	// p := obj.SumRange(i, j)
-	// fmt.Println(p)
+	//i := 0
+	//j := 2
+	//obj := Constructor(nums)
+	//p := obj.SumRange(i, j)
+	//fmt.Println(p)
 
-	// i := 2
-	// j := 5
-	// obj := Constructor(nums)
-	// p := obj.SumRange(i, j)
-	// fmt.Println(p)
-
-	i := 0
+	i := 2
 	j := 5
 	obj := Constructor(nums)
 	p := obj.SumRange(i, j)
 	fmt.Println(p)
+
+	//i := 0
+	//j := 5
+	//obj := Constructor(nums)
+	//p := obj.SumRange(i, j)
+	//fmt.Println(p)
 }
 
+// 构造时，创建时将原数组设置到字段中，SumRange 才计算最终结果
 type NumArray struct {
-	sums []int
+	nums []int
 }
 
 func Constructor(nums []int) NumArray {
-	sums := make([]int, len(nums)+1)
-	for i, v := range nums {
-		sums[i+1] = sums[i] + v
-	}
-	return NumArray{sums}
+	return NumArray{nums: nums}
 }
 
 func (na *NumArray) SumRange(i, j int) int {
-	return na.sums[j+1] - na.sums[i]
+	sum := 0
+	for index := i; index <= j; index++ {
+		sum += na.nums[index]
+	}
+	return sum
 }
+
+// 前缀和：构造的时候，将前n总和存储到数组中
+// 相比上面的方法，总循环次数是比较少的
+//type NumArray struct {
+//	sums []int
+//}
+//
+//func Constructor(nums []int) NumArray {
+//	sums := make([]int, len(nums)+1)
+//	for i, v := range nums {
+//		sums[i+1] = sums[i] + v
+//	}
+//	return NumArray{sums}
+//}
+//
+//func (na *NumArray) SumRange(i, j int) int {
+//	return na.sums[j+1] - na.sums[i]
+//}
