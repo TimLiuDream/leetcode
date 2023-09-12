b23323bc821a914679a6297a5ade727230d32f09
tim <timliudream@163.com>
2023-09-12 17:27:44 +0800

    update: 27 使用双指针解法

--- 27.RemoveElement/main.go
+++ 27.RemoveElement/main.go
@@ -22,9 +22,44 @@ func removeElement(nums []int, val int) int {
 	return len(nums)
 }
 
+// 双指针
+func removeElement1(nums []int, val int) int {
+	left, right := 0, 0
+	for ; right < len(nums); right++ {
+		if nums[right] != val {
+			nums[left] = nums[right]
+			left++
+		}
+	}
+	return left
+}
+
+// 双指针优化，使用头尾指针
+func removeElement2(nums []int, val int) int {
+	head, tail := 0, len(nums)
+	for head < tail {
+		if nums[head] == val {
+			nums[head] = nums[tail-1]
+			tail--
+		} else {
+			head++
+		}
+	}
+	return head
+}
+
 func main() {
 	nums := []int{3, 2, 2, 3}
-	fmt.Println(removeElement(nums, 3))
+	fmt.Println(removeElement1(nums, 3))
 	nums1 := []int{0, 1, 2, 2, 3, 0, 4, 2}
-	fmt.Println(removeElement(nums1, 2))
+	fmt.Println(removeElement1(nums1, 2))
+	nums2 := []int{}
+	fmt.Println(removeElement1(nums2, 2))
+	fmt.Println("--------------------------")
+	nums = []int{3, 2, 2, 3}
+	fmt.Println(removeElement2(nums, 3))
+	nums1 = []int{0, 1, 2, 2, 3, 0, 4, 2}
+	fmt.Println(removeElement2(nums1, 2))
+	nums2 = []int{1}
+	fmt.Println(removeElement2(nums2, 1))
 }
